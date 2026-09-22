4215cd8e27fdabe776dc0b4f6f0f3bb5e04402f3
Karen Etheridge <ether@cpan.org>
2026-09-22 15:59:38 -0700

    increment $VERSION after 0.31 release

--- lib/Package/Stash/XS.pm
+++ lib/Package/Stash/XS.pm
@@ -4,7 +4,7 @@ use warnings;
 use 5.008001;
 # ABSTRACT: Faster and more correct implementation of the Package::Stash API
 
-our $VERSION = '0.31';
+our $VERSION = '0.32';
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
